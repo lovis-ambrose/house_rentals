6aabc9fad5258580bea7713a0cb650e859d216d9
lovis-ambrose <lovisambrose212@gmail.com>
2024-09-11 15:20:20 +0300

    implement avatart

--- src/app/layout/navbar/avatar/avatar.component.ts
+++ src/app/layout/navbar/avatar/avatar.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import {Component, input, InputSignal} from '@angular/core';
+import {NgClass} from "@angular/common";
+import {FaIconComponent, FontAwesomeModule} from "@fortawesome/angular-fontawesome";
 
 @Component({
   selector: 'app-avatar',
   standalone: true,
-  imports: [],
+  imports: [
+    NgClass,
+    FontAwesomeModule,
+  ],
   templateUrl: './avatar.component.html',
   styleUrl: './avatar.component.scss'
 })
 export class AvatarComponent {
-
+  imageUrl: InputSignal<string | undefined> = input<string>();
+  avatarSize: InputSignal<"avatar-sm" | "avatar-xl" | undefined> = input<"avatar-sm" | "avatar-xl">();
 }
