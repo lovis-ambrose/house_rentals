c982b50d8f1d4012ca1806c9ca52053b6909885c
lovis-ambrose <lovisambrose212@gmail.com>
2024-09-11 14:52:35 +0300

    generate layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,31 @@
-import { Component } from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+import {FaIconLibrary, FontAwesomeModule} from "@fortawesome/angular-fontawesome";
+import {fontAwesomeIcons} from "./shared/font-awesome-icons";
+import {faAirbnb} from "@fortawesome/free-brands-svg-icons";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    ButtonModule,
+    FontAwesomeModule,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'Hostly';
+export class AppComponent implements OnInit {
+  falconLibrary: FaIconLibrary = inject(FaIconLibrary);
+
+  ngOnInit(): void {
+    this.initFontAwesome();
+  }
+
+  private initFontAwesome() {
+    this.falconLibrary.addIcons(...fontAwesomeIcons);
+  }
+
+  protected readonly faAirbnb = faAirbnb;
 }
---
+++ src/app/shared/font-awesome-icons.ts
@@ -0,0 +1,8 @@
+import {IconDefinition} from "@fortawesome/angular-fontawesome";
+import {faAirbnb} from "@fortawesome/free-brands-svg-icons";
+
+// optimize which icons to import
+export const fontAwesomeIcons: IconDefinition[] = [
+  faAirbnb,
+
+]
