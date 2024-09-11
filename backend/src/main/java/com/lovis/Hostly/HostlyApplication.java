8fba781b5cc14932e7f608e528169fa28401f945
lovis-ambrose <lovisambrose212@gmail.com>
2024-09-11 19:17:27 +0300

    initialize backend

---
+++ backend/src/main/java/com/lovis/Hostly/HostlyApplication.java
@@ -0,0 +1,13 @@
+package com.lovis.Hostly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HostlyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HostlyApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/lovis/Hostly/HostlyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lovis.Hostly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HostlyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
